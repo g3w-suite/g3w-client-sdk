fbc843cb50dbe401f12e1b50cac42f2cb1b8b880
volterra79 <francescoboccacci@libero.it>
2016-11-23 14:22:13 +0100

    aggiunti controlli dal branch validation

---
+++ g3w-ol3/src/controls/querybypolygoncontrol.js
@@ -0,0 +1,40 @@
+var utils = require('../utils');
+var InteractionControl = require('./interactioncontrol');
+
+var PickCoordinatesInteraction = require('../interactions/pickcoordinatesinteraction');
+
+var QueryByPolygonControl = function(options){
+  var self = this;
+  var _options = {
+    name: "querybypolygon",
+    tipLabel: "Query By Polygon",
+    label: "\uea0f",
+    interactionClass: PickCoordinatesInteraction
+  };
+  options = utils.merge(options,_options);
+  InteractionControl.call(this,options);
+};
+
+ol.inherits(QueryByPolygonControl, InteractionControl);
+
+var proto = QueryByPolygonControl.prototype;
+
+proto.setMap = function(map) {
+  var self = this;
+  InteractionControl.prototype.setMap.call(this,map);
+  this._interaction.on('boxstart',function(e){
+    self._startCoordinate = e.coordinate;
+  });
+
+  this._interaction.on('picked',function(e){
+    self.dispatchEvent({
+      type: 'picked',
+      coordinates: e.coordinate
+    });
+    if (self._autountoggle) {
+      self.toggle();
+    }
+  });
+};
+
+module.exports = QueryByPolygonControl;
