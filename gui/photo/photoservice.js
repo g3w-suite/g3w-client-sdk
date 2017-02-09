ff7b9f00fbc1d9816fa7d8b502d3f633359c43f3
volterra79 <francescoboccacci@libero.it>
2017-02-09 18:12:33 +0100

    rivisto componente photo. ora il servizio è "vuoto" e farà chiamtate s eil comnente lo vorrà non costruisce il componente (approccio sbagliato)

--- gui/photo/photoservice.js
+++ gui/photo/photoservice.js
@@ -1,22 +1,10 @@
 var inherit = require('core/utils/utils').inherit;
 var base = require('core/utils/utils').base;
 var G3WObject = require('core/g3wobject');
-var GUI = require('gui/gui');
-var PhotoComponent = require('gui/photo/vue/photo');
 
-function PhotoService(url) {
-  this.state = {
-    url: url
-  };
-  this.showFullPhoto = function() {
-    GUI.pushContent({
-      content: new PhotoComponent({
-        service: this
-      }),
-      backonclose: true,
-      closable: false
-    });
-  };
+function PhotoService(options) {
+  var options = options || {};
+  this.state = {};
   base(this);
 }
 
