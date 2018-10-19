0eaa26f7d55370530c7d97edd000da0e1e2af462
volterra79 <francescoboccacci@libero.it>
2018-10-19 15:17:54 +0200

    added method to insert i18n from plugins

--- core/i18n/i18n.service.js
+++ core/i18n/i18n.service.js
@@ -1,3 +1,13 @@
+// main object content for i18n
+const plugins18nConfig = {
+  it: {
+    plugins: {}
+  },
+  en: {
+    plugins: {}
+  }
+};
+
 function init(config) {
   i18next
   .use(i18nextXHRBackend)
@@ -7,25 +17,42 @@ function init(config) {
       fallbackLng: 'en',
       resources: config.resources
   });
+  return new Promise((resolve, reject) => {
+    jqueryI18next.init(i18next, $, {
+      tName: 't', // --> appends $.t = i18next.t
+      i18nName: 'i18n', // --> appends $.i18n = i18next
+      handleName: 'localize', // --> appends $(selector).localize(opts);
+      selectorAttr: 'data-i18n', // selector for translating elements
+      targetAttr: 'data-i18n-target', // element attribute to grab target element to translate (if diffrent then itself)
+      optionsAttr: 'data-i18n-options', // element attribute that contains options, will load/set if useOptionsAttr = true
+      useOptionsAttr: false, // see optionsAttr
+      parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text
+    });
+    addI18n(plugins18nConfig);
+    resolve();
+  })
 
-  jqueryI18next.init(i18next, $, {
-    tName: 't', // --> appends $.t = i18next.t
-    i18nName: 'i18n', // --> appends $.i18n = i18next
-    handleName: 'localize', // --> appends $(selector).localize(opts);
-    selectorAttr: 'data-i18n', // selector for translating elements
-    targetAttr: 'data-i18n-target', // element attribute to grab target element to translate (if diffrent then itself)
-    optionsAttr: 'data-i18n-options', // element attribute that contains options, will load/set if useOptionsAttr = true
-    useOptionsAttr: false, // see optionsAttr
-    parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text
-  });
 }
 
-const t = function(text){
+// function to translate
+const t = function(text) {
     return i18next.t(text);
 };
 
-const addI18n = function(i18nobject) {
-  Object.entries(i18nobject).forEach(([lng, value]) => {
+// function to translate plugins
+const tPlugin = function(text) {
+  return i18next.t(`plugins.${text}`);
+};
+
+const addI18nPlugin = function({name, config}) {
+  for (const language in config) {
+    plugins18nConfig[language].plugins[name] = config[language];
+  }
+  addI18n(plugins18nConfig);
+};
+
+const addI18n = function(i18nObject) {
+  Object.entries(i18nObject).forEach(([lng, value]) => {
     Object.keys(value).forEach((key) => {
       i18next.addResource(lng, 'translation', key, value[key])
     })
@@ -35,5 +62,7 @@ const addI18n = function(i18nobject) {
 module.exports = {
   init,
   t,
-  addI18n
+  tPlugin,
+  addI18n,
+  addI18nPlugin
 };
