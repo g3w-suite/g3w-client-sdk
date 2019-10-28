235c70c4e40ee4a0f1c9b5ecd5c4dc92fe97c286
volterra79 <francescoboccacci@libero.it>
2019-10-28 11:28:01 +0100

    merged with dev

--- config/dev/index.js
+++ config/dev/index.js
@@ -2,6 +2,9 @@ const ProjectsRegistry = require('core/project/projectsregistry');
 const ApplicationService = require('core/applicationservice');
 const GUI = require('gui/gui');
 
+// Handle ApplicationService on ready event
+ApplicationService.once('ready', function(){});
+
 // Handle project configuration to insert custom element on project
 ProjectsRegistry.oncebefore('setCurrentProject', function(project) {
   const wps = require('./wps/index');
@@ -10,5 +13,3 @@ ProjectsRegistry.oncebefore('setCurrentProject', function(project) {
 
 //Ready GUI
 GUI.once('ready', function(){});
-
-
