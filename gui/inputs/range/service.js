f9af4d905be503fe4392c37b0e05d589c445de02
volterra79 <francescoboccacci@libero.it>
2020-03-11 08:57:26 +0100

    refactgoring inputs

--- gui/inputs/range/service.js
+++ gui/inputs/range/service.js
@@ -5,7 +5,7 @@ const ValidatorClass = require('core/validators/inputs/range');
 
 function RangeService(options={}) {
   const {min, max} = options.state.input.options.values[0];
-  options.state.info = `(min: ${min} - max: ${max})`;
+  options.state.info = `[min: ${min} - max: ${max}]`;
   base(this, options);
   const validator = new ValidatorClass({
     min: 1*min,
